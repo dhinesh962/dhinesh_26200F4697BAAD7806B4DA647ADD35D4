57a3f1b1e4b1b1c6abc5caac0ee8d04de13cc161
dhinesh962 <144418675+dhinesh962@users.noreply.github.com>
2023-09-22 11:06:53 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,19 +1,24 @@
-class Player:
-    def play(self):
-        print("The player is playing cricket")
+def sort_students(student_list):
+    sorted_students = sorted(student_list, key=lambda x: x.cgpa, reverse=True)
+    return sorted_students
 
-class Batsman(Player):
-    def play(self):
-        print("The batsman is batting")
+# Define the Student class
+class Student:
+    def __init__(self, name, roll_number, cgpa):
+        self.name = name
+        self.roll_number = roll_number
+        self.cgpa = cgpa
 
-class Bowler(Player):
-    def play(self):
-        print("The bowler is bowling")
+# Test the function with a list of student objects
+students = [
+    Student("Alice", "A101", 3.9),
+    Student("Bob", "B102", 3.7),
+    Student("Charlie", "C103", 3.5),
+    Student("David", "D104", 3.8),
+]
 
-# Create objects of Batsman and Bowler classes
-batsman = Batsman()
-bowler = Bowler()
+sorted_students = sort_students(students)
 
-# Call the play() method for each object
-batsman.play()
-bowler.play()
+# Print the sorted list of students
+for student in sorted_students:
+    print(f"Name: {student.name}, Roll Number: {student.roll_number}, CGPA: {student.cgpa}")
--- main.py
+++ main.py
@@ -1,31 +1,18 @@
-class BankAccount:
-    def __init__(self, account_number, account_holder_name, initial_balance=0):
-        self.__account_number = account_number
-        self.__account_holder_name = account_holder_name
-        self.__account_balance = initial_balance
+def linear_search_product(products, target_product):
+    indices = []
+    for i in range(len(products)):
+        if products[i] == target_product:
+            indices.append(i)
+    return indices
+# Create a list of products
+products = ["Apple", "Banana", "Orange", "Apple", "Grapes", "Apple"]
 
-    def deposit(self, amount):
-        if amount > 0:
-            self.__account_balance += amount
-            return f"Deposited ${amount}. New balance: ${self.__account_balance}"
-        else:
-            return "Invalid deposit amount. Amount must be greater than 0."
+# Search for the target product "Apple"
+target_product = "Apple"
+result = linear_search_product(products, target_product)
 
-    def withdraw(self, amount):
-        if 0 < amount <= self.__account_balance:
-            self.__account_balance -= amount
-            return f"Withdrew ${amount}. New balance: ${self.__account_balance}"
-        else:
-            return "Invalid withdrawal amount or insufficient balance."
-
-    def display_balance(self):
-        return f"Account balance for {self.__account_holder_name}: ${self.__account_balance}"
-
-# Create an instance of the BankAccount class
-account = BankAccount("123456789", "John Doe", 1000)
-
-# Test deposit and withdrawal functionality
-print(account.display_balance())  # Display initial balance
-print(account.deposit(500))       # Deposit $500
-print(account.withdraw(200))      # Withdraw $200
-print(account.display_balance())  # Display updated balance
+# Print the result
+if len(result) > 0:
+    print(f"The product '{target_product}' was found at indices: {result}")
+else:
+    print(f"The product '{target_product}' was not found.")
